4c9b5335b585f68b2cd847d6e5f4267b420281ba
saadafzaldev <saaddevtec@gmail.com>
2024-11-25 14:47:27 +0530

    background video added

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,5 +1,5 @@
 import { useRef, useState } from "react"
-
+import Button from "./Button"
 export default function Hero() {
 
     const [currentIndex, setCurrentIndex] = useState(1)
@@ -7,15 +7,15 @@ export default function Hero() {
     const [isLoading, setIsLoading] = useState(true)
     const [loadedVideo, setLoadedVideo] = useState(0)
 
-    const totalVideos = 4
+    const totalVideos = 3
     const nextVideoRef = useRef(null)
 
 
         const miniVideoClickHandler = () => {   
 
             setHasClicked(true)
-            setCurrentIndex((prevIndex) => prevIndex + 1)
-
+            setCurrentIndex(upComingVideoIndex)
+            
         }
 
         const getVideoSource = (index:any ) => {
@@ -26,6 +26,9 @@ export default function Hero() {
             setLoadedVideo((prev) => prev + 1)
         }
 
+        const upComingVideoIndex = (currentIndex % totalVideos) + 1
+        
+
   return (
     <div className="relative h-dvh w-screen overflow-x-hidden">   
         <div id="video-container" className="relative z-10 h-dvh w-screen overflow-hidden rounded-lg bg-blue-75">
@@ -34,7 +37,7 @@ export default function Hero() {
                     <div onClick={miniVideoClickHandler} className="origin-center scale-50 opacity-0 transition-all duration-500 ease-in hover:scale-100 hover:opacity-100 cursor-pointer">
                            <video 
                            ref={nextVideoRef}
-                           src={getVideoSource(currentIndex + 1)!}
+                           src={getVideoSource(upComingVideoIndex)!}
                            loop
                            muted
                           id="currentVideo"
@@ -42,6 +45,39 @@ export default function Hero() {
                           onLoadedData={handleVideoLoad}
                           />
                     </div>
+                </div>
+                <video
+                    ref={nextVideoRef}
+                    src={getVideoSource(upComingVideoIndex)!}
+                    loop
+                    muted
+                    id="next-Video"
+                    className="absolute-center invisible absolute z-20 size-64 object-cover object-center"
+                    onLoadedData={handleVideoLoad}
+                />
+
+                <video
+                src={getVideoSource(currentIndex === totalVideos -1 ? 1 : currentIndex)!}
+                autoPlay
+                loop
+                muted
+                className="absolute left-0 top-0 size-full object-cover object-center"
+                onLoadedData={handleVideoLoad}
+                /> 
+            </div>
+            <h1 className="special-font hero-heading absolute bottom-5 right-5 z-40 text-blue-75">
+                G<b>a</b>ming
+            </h1>
+            <div className="absolute left-0 top-0 z-40 size-full">
+                <div className="mt-24 px-5 sm:px-10">
+                    <h1 className="special-font hero-heading text-blue-100">
+                         redefi<b>n</b>e
+                    </h1>
+                    <p className="mb-5 max-w-64 font-robert-regular text-blue-100">
+                        Enter the Metagame Layer <br /> Unleash the Play Economy
+                    </p>
+
+                    <Button/>
                 </div>
             </div>
         </div>
